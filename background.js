63412e73907c802a495137f28d63a1da3334b353
Pratham Pujya Sharma <prathamp.sharma.02@gmail.com>
2025-06-19 15:48:34 +0530

    Delete background.js

--- background.js
+++
@@ -1,8 +0,0 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
-  if (details.url.includes("linkedin.com/mynetwork")) {
-    chrome.scripting.executeScript({
-      target: { tabId: details.tabId },
-      files: ["content.js"]
-    });
-  }
-});
